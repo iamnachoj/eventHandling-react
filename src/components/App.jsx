493e64e8d64158a2ba0340e2c102459e7e905a5c
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-01-01 16:02:26 +0100

    submitClick function to set the heading name

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
 
 function App() {
-  const [headingText, setHeadingText] = useState("Hello");
-  function handlingClick() {
-    return setHeadingText("Submitted!");
+  const [name, setName] = useState("");
+  const [heading, setHeading] = useState("");
+  function changeInput(event) {
+    return setName(event.target.value);
+  }
+  function submitClick() {
+    return setHeading(name);
   }
   return (
     <div className="container">
-      <h1>{headingText}</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button onClick={handlingClick}> Submit</button>
+      <h1>Hello {heading}!</h1>
+      <input
+        onChange={changeInput}
+        type="text"
+        placeholder="What's your name?"
+      />
+      <button onClick={submitClick}>Submit</button>
     </div>
   );
 }
