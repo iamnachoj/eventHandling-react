36b48d925bf0a39e0523260373fe87f010d45a8d
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-01-01 15:41:46 +0100

    remove handling events from button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,28 +2,14 @@ import React, { useState } from "react";
 
 function App() {
   const [headingText, setHeadingText] = useState("Hello");
-  const [isMousedOver, setMousedOver] = useState(false);
   function handlingClick() {
     return setHeadingText("Submitted!");
-  }
-  function mouseOver() {
-    setMousedOver(true);
-  }
-  function mouseOut() {
-    setMousedOver(false);
   }
   return (
     <div className="container">
       <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button
-        style={{ backgroundColor: isMousedOver ? "black" : "white" }}
-        onClick={handlingClick}
-        onMouseOver={mouseOver}
-        onMouseOut={mouseOut}
-      >
-        Submit
-      </button>
+      <button onClick={handlingClick}> Submit</button>
     </div>
   );
 }
